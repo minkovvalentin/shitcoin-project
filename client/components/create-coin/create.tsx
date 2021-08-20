e8c63c8c0318102bca9a4f7ed9b28d4e42694775
Valentin Minkov <minkowalentin@gmail.com>
2021-08-21 00:46:02 +0200

    Added some styling to create component

--- client/components/create-coin/create.tsx
+++ client/components/create-coin/create.tsx
@@ -15,16 +15,58 @@ export default function CreateCoin() {
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-
-      <Input label="coinName" register={register} inputClassName={styles.coinName}/>
-      <Input label="abriviation" register={register} />
-      <Input label="contract" register={register} />
-      <Input label="network" register={register} />
-      <Input label="coinWebsite" register={register} />
-      <Input label="linkdin" register={register} />
-
-      <input type="submit" className={styles.button} />
-    </form>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <Input
+          label="coinName"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="abriviation"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="description"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="contract"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="network"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="coinWebsite"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="linkdin"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+          label="twitter"
+          register={register}
+          inputClassName={styles.input}
+        />
+        <Input
+        label="thumbnail"
+        register={register}
+        inputClassName={styles.input}
+        />
+        <input type="submit" className={styles.button} />
+      </form>
+      <div className={styles.infoContainer}>
+        Maybe preview coin you are entering here
+      </div>
+    </div>
   );
 }
--- client/interfaces/input.ts
+++ client/interfaces/input.ts
@@ -1,10 +1,13 @@
 export interface IFormValues {
   coinName: string;
   coinDescription: number;
+  description: string;
   abriviation: string;
   contract: string;
   network: string;
   coinWebsite:string;
   coinTwitter: string;
   linkdin: string;
+  twitter: string;
+  thumbnail: string;
 }
